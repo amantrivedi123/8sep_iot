0abb66beba1f5f8b0bdc057b17bf7e269a6080a3
amantrivedi123 <91320394+amantrivedi123@users.noreply.github.com>
2021-09-24 15:59:07 +0530

    Add files via upload

---
+++ CODE_2.ino
@@ -0,0 +1,28 @@
+void setup()
+{
+  // put your setup code here, to run once:
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12,1);
+  delay(100);
+  digitalWrite(12,0);
+  delay(100);
+  digitalWrite(12,1);
+  delay(100);
+  digitalWrite(12,0);
+  delay(100);
+  digitalWrite(12,1);
+  delay(100);
+  digitalWrite(12,0);
+  delay(100);
+  digitalWrite(12,1);
+  delay(100);
+  digitalWrite(12,0);
+  delay(100);
+  digitalWrite(12,1);
+  delay(100);}
